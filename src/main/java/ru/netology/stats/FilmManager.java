e57ad358438c500336d94670616f47f2d33cdd02
kip1k <motafaka@yandex.ru>
2023-11-16 05:48:30 +0300

    AddAllTest+LogicManager

--- src/main/java/ru/netology/stats/FilmManager.java
+++ src/main/java/ru/netology/stats/FilmManager.java
@@ -1,6 +1,16 @@
 package ru.netology.stats;
+
 public class FilmManager {
     private String[] films = new String[0];
+    private int limit;
+
+    public FilmManager() {
+        this.limit = 5;
+    }
+
+    public FilmManager(int limit) {
+        this.limit = limit;
+    }
 
     public void addFilm(String film) {
         String[] tmp = new String[films.length + 1];
@@ -12,10 +22,31 @@ public class FilmManager {
     }
 
     public String[] findAll() {
-        return films;
+        int sizeMassive;
+        if (films.length < limit) {
+            sizeMassive = films.length;
+        } else {
+            sizeMassive = limit;
+        }
+        String[] tmp = new String[sizeMassive];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[i];
+        }
+
+        return tmp;
     }
 
-    //public String[] findLast() {
-
-   // }
+    public String[] findLast() {
+        int sizeMassive;
+        if (films.length < limit) {
+            sizeMassive = films.length;
+        } else {
+            sizeMassive = limit;
+        }
+        String[] tmp = new String[sizeMassive];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+    }
 }
--- src/test/java/ru/netology/stats/FilmManagerTest.java
+++ src/test/java/ru/netology/stats/FilmManagerTest.java
@@ -3,15 +3,198 @@ package ru.netology.stats;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 public class FilmManagerTest {
     @Test
-    public void test() {
+    public void AddInAutoLimit() {
         FilmManager manager = new FilmManager();
         manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
 
-        String[] expected = { "Taxi 1" };
+
+        String[] expected = {"Taxi 1", "Taxi 2", "Taxi 3", "Taxi 4", "Taxi 5"};
         String[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void AddInAutoAboweLimit() {
+        FilmManager manager = new FilmManager();
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
+        manager.addFilm("Taxi 6");
+
+
+        String[] expected = {"Taxi 1", "Taxi 2", "Taxi 3", "Taxi 4", "Taxi 5"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void AddInAutoBeloweLimit() {
+        FilmManager manager = new FilmManager();
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+
+        String[] expected = {"Taxi 1", "Taxi 2", "Taxi 3", "Taxi 4"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void AddInManualLimit() {
+        FilmManager manager = new FilmManager(5);
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
+
+        String[] expected = {"Taxi 1", "Taxi 2", "Taxi 3", "Taxi 4", "Taxi 5" };
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void AddInManualAboweLimit() {
+        FilmManager manager = new FilmManager(5);
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
+        manager.addFilm("Taxi 6");
+
+
+        String[] expected = {"Taxi 1", "Taxi 2", "Taxi 3", "Taxi 4", "Taxi 5" };
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void AddInManualBeloweLimit() {
+        FilmManager manager = new FilmManager(5);
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+
+
+
+        String[] expected = {"Taxi 1", "Taxi 2", "Taxi 3", "Taxi 4",};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ManualLimit() {
+        FilmManager manager = new FilmManager(6);
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
+        manager.addFilm("Taxi 6");
+
+
+        String[] expected = {"Taxi 6", "Taxi 5", "Taxi 4", "Taxi 3", "Taxi 2", "Taxi 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void AboweManualLimit() {
+        FilmManager manager = new FilmManager(6);
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
+        manager.addFilm("Taxi 6");
+        manager.addFilm("Taxi 7");
+
+        String[] expected = {"Taxi 7", "Taxi 6", "Taxi 5", "Taxi 4", "Taxi 3", "Taxi 2"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void BeloweManualLimit() {
+        FilmManager manager = new FilmManager(6);
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
+
+
+        String[] expected = {"Taxi 5", "Taxi 4", "Taxi 3", "Taxi 2", "Taxi 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void AutoLimit() {
+        FilmManager manager = new FilmManager();
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
+
+
+        String[] expected = {"Taxi 5", "Taxi 4", "Taxi 3", "Taxi 2", "Taxi 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void AboweAutoLimit() {
+        FilmManager manager = new FilmManager();
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+        manager.addFilm("Taxi 5");
+        manager.addFilm("Taxi 6");
+
+
+        String[] expected = {"Taxi 6", "Taxi 5", "Taxi 4", "Taxi 3", "Taxi 2",};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void BeloweAutolLimit() {
+        FilmManager manager = new FilmManager();
+        manager.addFilm("Taxi 1");
+        manager.addFilm("Taxi 2");
+        manager.addFilm("Taxi 3");
+        manager.addFilm("Taxi 4");
+
+        String[] expected = {"Taxi 4", "Taxi 3", "Taxi 2", "Taxi 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
     }
 }
